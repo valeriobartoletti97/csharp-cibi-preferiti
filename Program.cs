778251a4bfe0dc0a1a9ce8053a9597e8cef21cae
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-16 16:54:05 +0200

    Exercise finished with bonus

--- Program.cs
+++ Program.cs
@@ -5,6 +5,31 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string[] FavouriteFood =
+            {
+                "Pizza",
+                "Carbonara",
+                "Tiramisu",
+                "Sushi",
+                "Fiorentina",
+            };
+            Console.WriteLine($"I miei piatti preferiti sono {FavouriteFood.Length}");
+            for (int i = 0; i < FavouriteFood.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}) {FavouriteFood[i]}");
+            }
+            Console.WriteLine($"Il mio cibo preferito è la {FavouriteFood[0]}");
+            Console.WriteLine($"Il mio cibo preferito ma non troppo è la {FavouriteFood[FavouriteFood.Length - 1]}");
+            if (FavouriteFood.Length % 2 == 0)
+            {
+                int median = FavouriteFood.Length / 2;
+                Console.WriteLine($"I cibi mediani sono {FavouriteFood[median]} e {FavouriteFood[median + 1]} ");
+            }
+            else
+            {
+                int median = FavouriteFood.Length / 2;
+                Console.WriteLine($"Il cibo mediano è {FavouriteFood[median]}");
+            }
         }
     }
 }

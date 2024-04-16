84dd498f2a8be0b265caeb126c0e6f98d585b6eb
Programmazione <Programmazione@DESKTOP-M9R5HET>
2024-04-16 16:52:29 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_cibi_preferiti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
